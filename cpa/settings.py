8cd37b4815b8898c9ec6301cecbd8f7f70e1946f
Zilong Zhao <zhaoz2017@my.fit.edu>
2018-11-25 10:25:34 -0500

    bootstrap added

--- cpa/settings.py
+++ cpa/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'cpa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cpadb',
+        'USER': 'postgres',
+        'PASSWORD': '12#qweasd',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -124,15 +128,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'assets'),
-)
-
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-try:
-    from local_setting import *
-except ImportError:
-    pass
+# try:
+#     from local_setting import *
+# except ImportError:
+#     pass
--- profiles/models.py
+++ profiles/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     email = models.EmailField(max_length=70)
     gender = models.CharField(max_length=20)
     date = models.DateTimeField(auto_now_add=True)
-    thumb = models.ImageField(default='default.png', blank=True)
+    image = models.ImageField(default='default.png', blank=True)
 
     def __str__(self):
         return self.first_name

b30c2f9486b995d2d140b800c72f08321bb1b8c6
Zilong Zhao <zhaoz2017@my.fit.edu>
2019-04-11 17:09:01 -0400

    delete f/l name

--- profiles/models.py
+++ profiles/models.py
@@ -2,12 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=70)
     gender = models.CharField(max_length=20)
     date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(default='default.png', blank=True)
+    starting_semester = models.CharField(max_length=20, blank=True)
+    starting_year = models.CharField(max_length=20, blank=True)
+    major = models.CharField(max_length=70, blank=True)
+    progplan = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.first_name
+
+class User_prog_plan(models.Model):
+    userid = models.CharField(max_length=30)
+    ureq_num = models.IntegerField()
+    met_flag = models.IntegerField()
+    usub = models.CharField(max_length=30)
+    unum = models.CharField(max_length=30)
+    uname = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.userid
--- progplan/models.py
+++ progplan/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Main_plan(models.Model):
+    mplan_name = models.CharField(max_length=50)
+    mreq_num = models.IntegerField()
+    msub = models.CharField(max_length=30)
+    mnum = models.CharField(max_length=30)
+    mname = models.CharField(max_length=30)
+    def __str__(self):
+        return self.userid
+
+class Option_plan(models.Model):
+    oplan_name = models.CharField(max_length=50)
+    oreq_num = models.IntegerField()
+    osub = models.CharField(max_length=30)
+    onum = models.CharField(max_length=30)
+    oname = models.CharField(max_length=30)
+    def __str__(self):
+        return self.oplan_name
